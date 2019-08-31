969d7feba834afb23d002efe9776974021bc480b
y0shek <kellerman.joshua.m@gmail.com>
2019-08-31 00:57:10 -0400

    better expanded windows

--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -18,10 +18,11 @@ export default class BlogPost extends React.Component {
     return (
       <div className="blog__post">
         <p onClick={this.toggleExpand} className="blog__post__time clickable">{this.props.post.date.split("T")[0]}</p>
-        <div onClick={this.toggleExpand} className="blog__post__body">
-          <h3 className="blog__post__title clickable" dangerouslySetInnerHTML={{ __html: this.props.post.title.rendered }}></h3>
-          <i class={!this.state.isExpanded ? "fas fa-plus-circle clickable" : "fas fa-minus-circle clickable"} ></i>
+        <div className="blog__post__body">
+          <h3 onClick={this.toggleExpand} className="blog__post__title clickable" dangerouslySetInnerHTML={{ __html: this.props.post.title.rendered }}></h3>
+          <i onClick={this.toggleExpand} class={!this.state.isExpanded ? "fas fa-plus-circle clickable" : "fas fa-minus-circle clickable"} ></i>
           <div className={this.state.isExpanded ? "blog__post__content expandedPost" : "blog__post__content"} dangerouslySetInnerHTML={{ __html: this.props.post.content.rendered }} />
+          <br />
         </div>
       </div >
     );

69302d82121dcea5fc168a866e660903ff6c1af2
y0shek <kellerman.joshua.m@gmail.com>
2019-08-31 01:05:24 -0400

    fixed blog again

--- src/templates/Blog/index.js
+++ src/templates/Blog/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import BlogPost from "../../components/BlogPost";
-import Construction from "../../components/Construction";
 import './style.css';
 
 export default class Blog extends React.Component {
@@ -8,26 +7,22 @@ export default class Blog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: [],
-      failed: false
+      posts: []
     };
   }
 
   componentWillMount = async () => {
     let response = await fetch(`https://wp.joshuamk.com/wp-json/wp/v2/posts/?filter[posts_per_page]=-1`);
     let data = await response.json();
-    if (this.state.posts.length === 0) {
-      this.setState({ posts: [], failed: true })
-    } else
-      this.setState({ posts: data, failed: false });
+    this.setState({ posts: data });
   }
 
   render() {
     return (
       <div className='blog'>
-        {!this.state.failed ? this.state.posts.map(post => (
+        {this.state.posts.map(post => (
           <BlogPost post={post} />
-        )) : <Construction theName="Blog" />}
+        ))}
       </div>
     );
   }

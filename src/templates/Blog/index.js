7d3168382efba5ca5d4f3080d03ce0e627be6bf5
y0shek <kellerman.joshua.m@gmail.com>
2019-08-31 01:00:46 -0400

    dont flash construction on blog

--- src/templates/Blog/index.js
+++ src/templates/Blog/index.js
@@ -8,20 +8,24 @@ export default class Blog extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
+      posts: [],
+      failed: false
     };
   }
 
   componentWillMount = async () => {
     let response = await fetch(`https://wp.joshuamk.com/wp-json/wp/v2/posts/?filter[posts_per_page]=-1`);
     let data = await response.json();
-    this.setState({ posts: data || [] });
+    if (this.state.posts.length === 0) {
+      this.setState({ posts: [], failed: true })
+    } else
+      this.setState({ posts: data, failed: false });
   }
 
   render() {
     return (
       <div className='blog'>
-        {this.state.posts.length !== 0 ? this.state.posts.map(post => (
+        {!this.state.failed ? this.state.posts.map(post => (
           <BlogPost post={post} />
         )) : <Construction theName="Blog" />}
       </div>

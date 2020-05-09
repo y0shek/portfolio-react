6b0c6703177f2610f3a60cd885e645cfb43325a1
Joshua Kellerman <y0shek@Ch40s.local>
2020-05-08 23:03:11 -0700

    made contact page simpler

--- src/components/ContactMe/index.js
+++ src/components/ContactMe/index.js
@@ -5,9 +5,7 @@ export default class ContactMe extends React.Component {
   render() {
     return (
       <div className="contactInfo">
-        <div className="contactInfo__content">
-          <iframe className="contactInfo_googleForm" src="https://docs.google.com/forms/d/e/1FAIpQLSeTjrFCrfLFHEIew7O8q8NoarhzjzF60WmQz8cCi1a6u5L0Tw/viewform?embedded=true" height="1024" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-        </div>
+          <iframe className="contactInfo_googleForm" src="https://docs.google.com/forms/d/e/1FAIpQLSeTjrFCrfLFHEIew7O8q8NoarhzjzF60WmQz8cCi1a6u5L0Tw/viewform?embedded=true" height="1024" width="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
       </div>
     );
   }

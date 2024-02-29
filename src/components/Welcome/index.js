0d7ae4ee978c0479e0be7f4e77bfa1ae2392fefd
Joshua Kellerman <kellerman.joshua.m@gmail.com>
2024-02-29 13:27:41 -0500

    new video

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -43,7 +43,7 @@ export default class Welcome extends React.Component {
         autoplay: 1,
         loop: 1,
         mute: 1,
-        playlist: "xjCzu5uWnE0",
+        playlist: "O5EcPaXcZSU",
         controls: 0
       }
     }
@@ -58,7 +58,7 @@ export default class Welcome extends React.Component {
             <div>
               <div className="welcome__videoCover" />
               <div id="vid" className="welcome__videoScreen">
-                <YouTube videoId="xjCzu5uWnE0" className="welcome__video" opts={opts} ></YouTube>
+                <YouTube videoId="O5EcPaXcZSU" className="welcome__video" opts={opts} ></YouTube>
               </div>
             </div>
           )

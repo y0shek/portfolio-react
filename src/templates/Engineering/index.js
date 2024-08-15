216315669570dea6d2d2a86f52567d78ce339c7e
Joshua Kellerman <kellerman.joshua.m@gmail.com>
2024-08-15 17:43:05 -0400

    new resume

--- src/templates/Engineering/index.js
+++ src/templates/Engineering/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Section from "../../components/Section";
 import myPic from '../../images/me_prof.jpg';
-import myResume from "../../pdfs/Kellerman_resume_2024_web.pdf";
+import myResume from "../../pdfs/Kellerman_resume_2024_tech_web.pdf";
 import { SocialIcon } from "react-social-icons";
 import "./style.css";
 

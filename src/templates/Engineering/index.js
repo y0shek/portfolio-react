193ab518906c59d0732e23c55a289e28362df800
Joshua Kellerman <kellerman.joshua.m@gmail.com>
2026-05-19 18:15:35 -0700

    actually

--- src/templates/Engineering/index.js
+++ src/templates/Engineering/index.js
@@ -67,11 +67,11 @@ export default class Portfolio extends React.Component {
             A Lesson from Disney Animation for More Human-Like AI Agents
           </h5>
           <a
-            href="https://joshua-kellerman.medium.com/what-if-your-companys-agents-are-double-agents-3e067e8dfab8?postPublishedType=repub"
+            href="https://joshua-kellerman.medium.com/what-if-your-companys-agents-are-double-agents-3e067e8dfab8"
             target="_blank"
           >
             <h4 style={{ "margin-bottom": 0 }}>
-              Are Your AI Agents Double-Agents? (Medium.com)
+              Are Your AI Agents Actually Double-Agents? (Medium.com)
             </h4>
           </a>
           <h5 style={{ "margin-top": "10px", "margin-bottom": "0" }}>
